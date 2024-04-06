920a06190e1fdc1698546e129c45c55487b66efa
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-06 20:30:51 +0800

    types.h: add minimum and maximum value about different types.

--- include/types.h
+++ include/types.h
@@ -1,3 +1,4 @@
+// reference basic types from Go
 typedef char      int8;
 typedef short     int16;
 typedef int       int32;
@@ -28,3 +29,17 @@ typedef int32 rune;
 #define FALSE 0;
 
 #define NULL 0;
+
+// copy from <stdint.h>
+#define INT8_MIN   (-127i8 - 1)
+#define INT16_MIN  (-32767i16 - 1)
+#define INT32_MIN  (-2147483647i32 - 1)
+#define INT64_MIN  (-9223372036854775807i64 - 1)
+#define INT8_MAX   127i8
+#define INT16_MAX  32767i16
+#define INT32_MAX  2147483647i32
+#define INT64_MAX  9223372036854775807i64
+#define UINT8_MAX  0xffui8
+#define UINT16_MAX 0xffffui16
+#define UINT32_MAX 0xffffffffui32
+#define UINT64_MAX 0xffffffffffffffffui64
