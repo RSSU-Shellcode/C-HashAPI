8d08919579a84d9dac2c51c8fbb8527d7f679381
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-08 17:11:42 +0800

    win_exec: add a example for execute calculator.

---
+++ example/win_exec/win_exec.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "x64/hash_api.h"
+
+typedef uint(*WinExec)(char* lpCmdLine, uint uCmdShow);
+
+#pragma comment(linker, "/ENTRY:EntryMain")
+uint64 EntryMain() {    
+    uint64 hash = 0xCA2DBA870B222A04;
+    uint64 key  = 0xB725F01C80CE0985;
+    WinExec winExec = (WinExec)FindAPI(hash, key);
+    if (winExec == 0)
+    {
+        return 0;
+    }
+    char cmd[] = { 'c', 'a', 'l', 'c', '.', 'e', 'x', 'e', 0 };
+    return winExec(&cmd[0], 1);
+}
