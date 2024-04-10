76b7aa708191aa3cf2e4317f3f54fc91ce86a419
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-10 14:18:18 +0800

    win_exec/win_exec.c: replace  if (winExec == 0) to if (winExec == NULL).

--- example/win_exec/win_exec.c
+++ example/win_exec/win_exec.c
@@ -5,13 +5,13 @@ typedef uint(*WinExec)(char* lpCmdLine, uint uCmdShow);
 
 #pragma comment(linker, "/ENTRY:EntryMain")
 uint64 EntryMain() {    
-    uint64 hash = 0xCA2DBA870B222A04;
-    uint64 key  = 0xB725F01C80CE0985;
+    uint64 hash = 0x4BBDC64FE359FC5A;
+    uint64 key  = 0xD7D68112BE34E158;
     WinExec winExec = (WinExec)FindAPI(hash, key);
-    if (winExec == 0)
+    if (winExec == NULL)
     {
         return 0;
     }
-    char cmd[] = { 'c', 'a', 'l', 'c', '.', 'e', 'x', 'e', 0 };
+    char cmd[] = {'c', 'a', 'l', 'c', '.', 'e', 'x', 'e', 0};
     return winExec(&cmd[0], 1);
 }
