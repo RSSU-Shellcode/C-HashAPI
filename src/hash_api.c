805adc5fbc06f5365a1edcc494e2ee0436e8f6f3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-11 18:07:19 +0800

    hash_api.c: finish FindAPI for x86, improve code style.

--- src/hash_api.c
+++ src/hash_api.c
@@ -137,50 +137,46 @@ static uint32 ror32(uint32 value, uint32 bits);
 // FindAPI is used to FindAPI address by hash and key.
 uintptr FindAPI(uint32 hash, uint32 key) {
     uint32 seedHash = calcSeedHash(key);
-    uint32 keyHash = calcKeyHash(seedHash, key);
+    uint32 keyHash  = calcKeyHash(seedHash, key);
 
-    uintptr peb = __readgsqword(96);
-    uintptr ldr = *(uintptr*)(peb + 24);
-    uintptr mod = *(uintptr*)(ldr + 32);
+    uintptr peb = __readfsdword(48);
+    uintptr ldr = *(uintptr*)(peb + 12);
+    uintptr mod = *(uintptr*)(ldr + 20);
 
     for (;; mod = *(uintptr*)(mod)) {
-        uintptr modName = *(uintptr*)(mod + 80);
+        uintptr modName = *(uintptr*)(mod + 40);
         if (modName == 0x00) {
             break;
         }
-        uintptr modBase = *(uintptr*)(mod + 32);
+        uintptr modBase  = *(uintptr*)(mod + 16);
         uintptr peHeader = modBase + *(uint32*)(modBase + 60);
-        // check this module actually a PE64 executable
-        if (*(uint16*)(peHeader + 24) != 0x020B) {
-            continue;
-        }
         // get RVA of export address tables(EAT)
-        uint32 eatRVA = *(uint32*)(peHeader + 136);
+        uint32 eatRVA = *(uint32*)(peHeader + 120);
         if (eatRVA == 0) {
             continue;
         }
         uintptr eat = modBase + eatRVA;
         // calculate module name hash
-        uint64 modHash = seedHash;
-        uint16 nameLen = *(uint16*)(mod + 74);
+        uint32 modHash = seedHash;
+        uint16 nameLen = *(uint16*)(mod + 38);
         for (uint16 i = 0; i < nameLen; i++) {
             byte b = *(byte*)(modName + i);
             if (b >= 'a') {
                 b -= 0x20;
             }
-            modHash = ror64(modHash, ROR_MOD);
+            modHash = ror32(modHash, ROR_MOD);
             modHash += b;
         }
         // calcualte function name hash
-        uint32 numFunc = *(uint32*)(eat + 24);
+        uint32  numFunc   = *(uint32*)(eat + 24);
         uintptr funcNames = modBase + *(uint32*)(eat + 32);
         for (uint32 i = 0; i < numFunc; i++) {
             // calculate function name address
-            byte* funcName = (byte*)(modBase + *(uint32*)(funcNames + i * 4));
-            uint64 funcHash = seedHash;
+            byte*  funcName = (byte*)(modBase + *(uint32*)(funcNames + i * 4));
+            uint32 funcHash = seedHash;
             for (;;) {
                 byte b = *funcName;
-                funcHash = ror64(funcHash, ROR_FUNC);
+                funcHash = ror32(funcHash, ROR_FUNC);
                 funcHash += b;
                 if (b == 0x00) {
                     break;
@@ -188,7 +184,7 @@ uintptr FindAPI(uint32 hash, uint32 key) {
                 funcName++;
             }
             // calculate the finally hash and compare it
-            uint64 h = seedHash + keyHash + modHash + funcHash;
+            uint32 h = seedHash + keyHash + modHash + funcHash;
             if (h != hash) {
                 continue;
             }
@@ -207,8 +203,8 @@ uintptr FindAPI(uint32 hash, uint32 key) {
 
 static uint32 calcSeedHash(uint32 key) {
     uint32 hash = key;
-    byte* ptr = (byte*)(&key);
-    for (int i = 0; i < 8; i++) {
+    byte*  ptr  = (byte*)(&key);
+    for (int i = 0; i < 4; i++) {
         hash = ror32(hash, ROR_SEED);
         hash += *ptr;
         ptr++;
@@ -218,8 +214,8 @@ static uint32 calcSeedHash(uint32 key) {
 
 static uint32 calcKeyHash(uint32 seed, uint32 key) {
     uint32 hash = seed;
-    byte* ptr = (byte*)(&key);
-    for (int i = 0; i < 8; i++) {
+    byte*  ptr  = (byte*)(&key);
+    for (int i = 0; i < 4; i++) {
         hash = ror32(hash, ROR_KEY);
         hash += *ptr;
         ptr++;
