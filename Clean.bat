fab962b185e5b54fa38e89a5fa7fd3de01a922d9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-11 18:19:09 +0800

    remove Release in root and example/win_exec directory.

--- Clean.bat
+++ Clean.bat
@@ -2,7 +2,11 @@ del /S /Q ".vs\C-HashAPI\v17\ipch\AutoPCH"
 del /S /Q ".vs\C-HashAPI\v17\Browse.VC.db"
 
 rd /S /Q "Debug"
+rd /S /Q "Release"
 rd /S /Q "x86"
 rd /S /Q "x64"
+
 rd /S /Q "example\win_exec\x86"
 rd /S /Q "example\win_exec\x64"
+rd /S /Q "example\win_exec\Debug"
+rd /S /Q "example\win_exec\Release"
