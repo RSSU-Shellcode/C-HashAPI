afd7bf0d3f9a9e3be414b3e6640c1fac1ebaa240
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-13 14:31:12 +0800

    hash_api.h: replace type name FindAPI_F to FindAPI_t.

--- include/hash_api.h
+++ include/hash_api.h
@@ -5,14 +5,14 @@
 
 #ifdef _WIN64
 
-typedef uintptr (*FindAPI_F)(uint64 hash, uint64 key);
+typedef uintptr (*FindAPI_t)(uint64 hash, uint64 key);
 
 // FindAPI is used to FindAPI address by hash and key.
 uintptr FindAPI(uint64 hash, uint64 key);
 
 #elif _WIN32
 
-typedef uintptr (*FindAPI_F)(uint32 hash, uint32 key);
+typedef uintptr (*FindAPI_t)(uint32 hash, uint32 key);
 
 // FindAPI is used to FindAPI address by hash and key.
 uintptr FindAPI(uint32 hash, uint32 key);
