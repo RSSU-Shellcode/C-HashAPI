a4f558f05d6599bcabb36bd34a81b972621a3d93
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-05 15:42:08 +0800

    hash_api.h: add HashAPI_A and HashAPI_W for calculate Windows API hash.

--- include/hash_api.h
+++ include/hash_api.h
@@ -8,4 +8,12 @@ typedef uintptr (*FindAPI_t)(uint hash, uint key);
 // FindAPI is used to FindAPI address by hash and key.
 uintptr FindAPI(uint hash, uint key);
 
+// HashAPI is used to calculate Windows API hash by module
+// and function with key, module and function are ASCII.
+uint HashAPI_A(byte* module, byte* function, uint key);
+
+// HashAPI is used to calculate Windows API hash by module
+// and function with key, module is Unicode, function is ASCII.
+uint HashAPI_W(byte* module, byte* function, uint key);
+
 #endif // HASH_API_H
