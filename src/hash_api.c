e7f988fdbf6168fb805687d892d29fca6556f11a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-02 19:34:18 +0800

    hash_api.c: use mem_clean for prevent incorrect compiler optimize.

--- src/hash_api.c
+++ src/hash_api.c
@@ -144,8 +144,11 @@ void* FindAPI(uint hash, uint key)
                 }
                 src++;
             }
-            // build DLL name
+            // use "mem_clean" for prevent incorrect compiler
+            // optimize and generate incorrect shellcode
             byte dllName[512];
+            mem_clean(&dllName, sizeof(dllName));
+            // build DLL name
             mem_copy(&dllName[0], exportName, dot + 1);
             dllName[dot+1] = 'd';
             dllName[dot+2] = 'l';
