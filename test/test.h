cf8ee5d4a07027a5cb8b51c3f416410417ce2e5e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-03 14:04:37 +0800

    test.h: add #pragma warning(disable: 4276) for ignore warning, add TestLibString.

--- test/test.h
+++ test/test.h
@@ -3,16 +3,24 @@
 
 #include "c_types.h"
 
+// define unit tests
+#pragma warning(push)
+#pragma warning(disable: 4276)
+bool TestLibString();
+
 bool TestHashAPI();
 bool TestHashAPI64();
 bool TestHashAPI32();
 bool TestFindAPI();
 bool TestForwarded();
+#pragma warning(pop)
 
 typedef bool (*test_t)();
 typedef struct { byte* Name; test_t Test; } unit;
 
 static unit tests[] = {
+    { "Lib_String", TestLibString },
+
     { "HashAPI",   TestHashAPI   },
     { "HashAPI64", TestHashAPI64 },
     { "HashAPI32", TestHashAPI32 },
