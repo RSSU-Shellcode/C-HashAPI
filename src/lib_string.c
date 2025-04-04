db32c4492a2e0ced402af2900f3f7add6a467156
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-04 14:29:22 +0800

    lib_string.c: add more functions about strncmp, strcpy and strncpy.

--- src/lib_string.c
+++ src/lib_string.c
@@ -1,15 +1,13 @@
 #include "c_types.h"
 #include "lib_string.h"
 
-// Optimization of this library must be disabled,
-// otherwise when using builder to build shellcode,
-// the compiler will mistakenly skip the following
-// functions and instead use <stdio.h> or built-in
-// functions, causing the function address in the
-// shellcode to be incorrect.
-#pragma optimize("", off)
+#pragma optimize("t", on)
 
-uint strlen_a(ascii s)
+byte   lowercase_a(byte c);
+uint16 lowercase_w(uint16 c);
+
+__declspec(noinline)
+uint strlen_a(ANSI s)
 {
     uint l = 0;
     for (;;)
@@ -24,7 +22,8 @@ uint strlen_a(ascii s)
     return l;
 }
 
-uint strlen_w(utf16 s)
+__declspec(noinline)
+uint strlen_w(UTF16 s)
 {
     uint l = 0;
     for (;;)
@@ -39,7 +38,8 @@ uint strlen_w(utf16 s)
     return l;
 }
 
-int strcmp_a(ascii a, ascii b)
+__declspec(noinline)
+int strcmp_a(ANSI a, ANSI b)
 {
     for (;;)
     {
@@ -64,7 +64,8 @@ int strcmp_a(ascii a, ascii b)
     }
 }
 
-int strcmp_w(utf16 a, utf16 b)
+__declspec(noinline)
+int strcmp_w(UTF16 a, UTF16 b)
 {
     for (;;)
     {
@@ -90,4 +91,267 @@ int strcmp_w(utf16 a, utf16 b)
     }
 }
 
-#pragma optimize("", on)
+__declspec(noinline)
+int strncmp_a(ANSI a, ANSI b, int64 n)
+{
+    for (int64 i = 0; i < n; i++)
+    {
+        byte s0 = *a;
+        byte s1 = *b;
+        if (s0 == s1)
+        {
+            if (s0 == 0x00)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+__declspec(noinline)
+int strncmp_w(UTF16 a, UTF16 b, int64 n)
+{
+    for (int64 i = 0; i < n; i++)
+    {
+        uint16 s0 = *a;
+        uint16 s1 = *b;
+        if (s0 == s1)
+        {
+            if (s0 == 0x0000)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+__declspec(noinline)
+int stricmp_a(ANSI a, ANSI b)
+{
+    for (;;)
+    {
+        byte s0 = lowercase_a(*a);
+        byte s1 = lowercase_a(*b);
+        if (s0 == s1)
+        {
+            if (s0 == 0x00)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
+
+__declspec(noinline)
+int stricmp_w(UTF16 a, UTF16 b)
+{
+    for (;;)
+    {
+        uint16 s0 = lowercase_w(*a);
+        uint16 s1 = lowercase_w(*b);
+        if (s0 == s1)
+        {
+            if (s0 == 0x0000)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else
+        {
+            return -1;
+        }
+    }
+}
+
+__declspec(noinline)
+int strnicmp_a(ANSI a, ANSI b, int64 n)
+{
+    for (int64 i = 0; i < n; i++)
+    {
+        byte s0 = lowercase_a(*a);
+        byte s1 = lowercase_a(*b);
+        if (s0 == s1)
+        {
+            if (s0 == 0x00)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+__declspec(noinline)
+int strnicmp_w(UTF16 a, UTF16 b, int64 n)
+{
+    for (int64 i = 0; i < n; i++)
+    {
+        uint16 s0 = lowercase_w(*a);
+        uint16 s1 = lowercase_w(*b);
+        if (s0 == s1)
+        {
+            if (s0 == 0x0000)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+__declspec(noinline)
+byte lowercase_a(byte c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        c += 0x20;
+    }
+    return c;
+}
+
+__declspec(noinline)
+uint16 lowercase_w(uint16 c)
+{
+    if (c >= L'A' && c <= L'Z')
+    {
+        c += 0x20;
+    }
+    return c;
+}
+
+__declspec(noinline)
+uint strcpy_a(ANSI dst, ANSI src)
+{
+    uint l = 0;
+    for (;;)
+    {
+        byte s = *src;
+        *dst = s;
+        if (s == 0x00)
+        {
+            break;
+        }
+
+        l++;
+        dst++;
+        src++;
+    }
+    return l;
+}
+
+__declspec(noinline)
+uint strcpy_w(UTF16 dst, UTF16 src)
+{
+    uint l = 0;
+    for (;;)
+    {
+        uint16 s = *src;
+        *dst = s;
+        if (s == 0x0000)
+        {
+            break;
+        }
+
+        l++;
+        dst++;
+        src++;
+    }
+    return l;
+}
+
+__declspec(noinline)
+uint strncpy_a(ANSI dst, ANSI src, int64 n)
+{
+    uint l = 0;
+    for (int64 i = 0; i < n; i++)
+    {
+        byte s = *src;
+        *dst = s;
+        if (s == 0x00)
+        {
+            break;
+        }
+
+        l++;
+        dst++;
+        src++;
+    }
+    return l;
+}
+
+__declspec(noinline)
+uint strncpy_w(UTF16 dst, UTF16 src, int64 n)
+{
+    uint l = 0;
+    for (int64 i = 0; i < n; i++)
+    {
+        uint16 s = *src;
+        *dst = s;
+        if (s == 0x0000)
+        {
+            break;
+        }
+
+        l++;
+        dst++;
+        src++;
+    }
+    return l;
+}
+
+#pragma optimize("t", off)
