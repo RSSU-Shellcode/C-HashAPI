2785bb42aaccc174fa8eb102df83234987b84c5e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-09-05 14:18:55 +0800

    win_exec/win_exec.c: rewrite EntryMain about FindAPI usage.

--- example/win_exec/win_exec.c
+++ example/win_exec/win_exec.c
@@ -5,17 +5,19 @@
 #pragma comment(linker, "/ENTRY:EntryMain")
 uint EntryMain() {
 #ifdef _WIN64
-    uint hash = 0x4BBDC64FE359FC5A;
-    uint key  = 0xD7D68112BE34E158;
+    uint mHash = 0x2A5175AD1A0CECBC;
+    uint pHash = 0x6596B31A1F68D830;
+    uint hKey  = 0x7A61A1C72F518C54;
 #elif _WIN32
-    uint hash = 0x0AE20914;
-    uint key  = 0x61DA2999;
+    uint mHash = 0x42509A1C;
+    uint pHash = 0x3CA3C21A;
+    uint hKey  = 0xCADE960B;
 #endif
-    WinExec_t winExec = FindAPI(hash, key);
-    if (winExec == NULL)
+    WinExec_t WinExec = FindAPI(mHash, pHash, hKey);
+    if (WinExec == NULL)
     {
         return 1;
     }
     char cmd[] = {'c', 'a', 'l', 'c', '.', 'e', 'x', 'e', 0};
-    return winExec(cmd, 1);
+    return WinExec(cmd, 1);
 }
